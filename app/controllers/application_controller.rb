095f6efa5808a3d08ba749deef9b1007f3fea79e
Jason Baik <baikjs87@gmail.com>
2022-08-08 20:10:35 -0400

    Complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < Sinatra::Base
 
-  # add routes
+  set :default_content_type, 'application/json'
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakeries = Bakery.find(params[:id])
+    bakeries.to_json(only: [:name, :baked_goods], include: {baked_goods: {only: [:name, :price]}})
+  end
+
+  get '/baked_goods/by_price' do
+    baked = BakedGood.all.order(price: :desc)
+    baked.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    baked = BakedGood.all.order(price: :desc).first
+    baked.to_json
+  end
 
 end
